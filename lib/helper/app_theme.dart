2abe2da32526e95b01a051b8505746816fd483ec
Rafi1702 <108681282+Rafi1702@users.noreply.github.com>
2024-08-17 15:46:06 +0700

    add flex-color-scheme package

--- lib/helper/app_theme.dart
+++ lib/helper/app_theme.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -5,18 +6,33 @@ class AppTheme {
 
   static const gridBoxColor = Color(0xFF212121);
 
-  static final darkTheme = ThemeData(
-    appBarTheme: const AppBarTheme(elevation: 1.0),
-    colorScheme: ColorScheme(
+  static final darkTheme = FlexThemeData.dark(
+    colorScheme: const ColorScheme(
       brightness: Brightness.dark,
-      primary: Colors.green,
-      secondary: const Color(0xFFFFA48E),
-      error: Colors.red.shade500,
-      onError: Colors.white,
-      onPrimary: Colors.white,
-      onSecondary: Colors.black,
-      onSurface: Colors.white,
-      surface: const Color(0xFF121212),
+      primary: Color(0xff5db3d5),
+      onPrimary: Color(0xff101b1e),
+      primaryContainer: Color(0xff297ea0),
+      onPrimaryContainer: Color(0xffd9edf5),
+      secondary: Color(0xffa1e9df),
+      onSecondary: Color(0xff181e1e),
+      secondaryContainer: Color(0xff005049),
+      onSecondaryContainer: Color(0xffd0e3e1),
+      tertiary: Color(0xffa0e5e5),
+      onTertiary: Color(0xff181e1e),
+      tertiaryContainer: Color(0xff004f50),
+      onTertiaryContainer: Color(0xffd0e2e3),
+      error: Color(0xffcf6679),
+      onError: Color(0xff1e1214),
+      errorContainer: Color(0xffb1384e),
+      onErrorContainer: Color(0xfff9dde2),
+      outline: Color(0xff959999),
+      surface: Color(0xff131516),
+      onSurface: Color(0xfff1f1f1),
+      onSurfaceVariant: Color(0xffe3e3e4),
+      inverseSurface: Color(0xfffafcfd),
+      onInverseSurface: Color(0xff0e0e0e),
+      inversePrimary: Color(0xff355967),
+      shadow: Color(0xff000000),
     ),
   );
 
